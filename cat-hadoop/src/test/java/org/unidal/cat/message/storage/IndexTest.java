3dc28c0531631f3e2f70e1e7f16054a82c3806d7
MaorSagi <maors802@gmail.com>
2020-08-16 17:30:41 +0300

    code formatting

--- cat-hadoop/src/test/java/org/unidal/cat/message/storage/IndexTest.java
+++ cat-hadoop/src/test/java/org/unidal/cat/message/storage/IndexTest.java
@@ -31,67 +31,67 @@ import com.dianping.cat.message.internal.MessageId;
 
 public class IndexTest extends ComponentTestCase {
 
-	private String m_ip;
+    private String m_ip;
 
-	@Before
-	public void before() throws Exception {
-		StorageConfiguration config = lookup(StorageConfiguration.class);
+    @Before
+    public void before() throws Exception {
+        StorageConfiguration config = lookup(StorageConfiguration.class);
 
-		config.setBaseDataDir(new File("target"));
-		File baseDir = new File("target");
-		Files.forDir().delete(new File(baseDir, "dump"), true);
-		m_ip = NetworkInterfaceManager.INSTANCE.getLocalHostAddress();
-	}
+        config.setBaseDataDir(new File("target"));
+        File baseDir = new File("target");
+        Files.forDir().delete(new File(baseDir, "dump"), true);
+        m_ip = NetworkInterfaceManager.INSTANCE.getLocalHostAddress();
+    }
 
-	@Test
-	public void testMapAndLookups() throws Exception {
-		int total = 15000;
-		IndexManager manager = lookup(IndexManager.class, "local");
-		Index index = manager.getIndex("from", m_ip, 403899, true);
+    @Test
+    public void testMapAndLookups() throws Exception {
+        int total = 15000;
+        IndexManager manager = lookup(IndexManager.class, "local");
+        Index index = manager.getIndex("from", m_ip, 403899, true);
 
-		for (int i = 1; i < total; i++) {
-			MessageId from = MessageId.parse("from-0a260014-403899-" + i);
-			MessageId to = MessageId.parse("to-0a260015-403899-" + i);
+        for (int i = 1; i < total; i++) {
+            MessageId from = MessageId.parse("from-0a260014-403899-" + i);
+            MessageId to = MessageId.parse("to-0a260015-403899-" + i);
 
-			index.map(from, to);
-		}
+            index.map(from, to);
+        }
 
-		index = manager.getIndex("from", m_ip, 403899, true);
-		for (int i = 1; i < total; i++) {
-			MessageId from = MessageId.parse("from-0a260014-403899-" + i);
-			MessageId expected = MessageId.parse("to-0a260015-403899-" + i);
+        index = manager.getIndex("from", m_ip, 403899, true);
+        for (int i = 1; i < total; i++) {
+            MessageId from = MessageId.parse("from-0a260014-403899-" + i);
+            MessageId expected = MessageId.parse("to-0a260015-403899-" + i);
 
-			MessageId actual = index.find(from);
-			Assert.assertEquals(expected, actual);
-		}
-	}
+            MessageId actual = index.find(from);
+            Assert.assertEquals(expected, actual);
+        }
+    }
 
-	@Test
-	public void testMapAndLookupManyIps() throws Exception {
-		IndexManager manager = lookup(IndexManager.class, "local");
-		Index index = manager.getIndex("from", m_ip, 403899, true);
+    @Test
+    public void testMapAndLookupManyIps() throws Exception {
+        IndexManager manager = lookup(IndexManager.class, "local");
+        Index index = manager.getIndex("from", m_ip, 403899, true);
 
-		for (int i = 1; i < 150000; i++) {
-			for (int ip = 0; ip < 10; ip++) {
-				MessageId from = MessageId.parse("from-0a26000" + ip + "-403899-" + i);
-				MessageId to = MessageId.parse("from-0a25000" + ip + "-403899-" + i);
+        for (int i = 1; i < 150000; i++) {
+            for (int ip = 0; ip < 10; ip++) {
+                MessageId from = MessageId.parse("from-0a26000" + ip + "-403899-" + i);
+                MessageId to = MessageId.parse("from-0a25000" + ip + "-403899-" + i);
 
-				index.map(from, to);
-			}
-		}
+                index.map(from, to);
+            }
+        }
 
-		index = manager.getIndex("from", m_ip, 403899, true);
-		for (int i = 1; i < 150000; i++) {
+        index = manager.getIndex("from", m_ip, 403899, true);
+        for (int i = 1; i < 150000; i++) {
 
-			for (int ip = 0; ip < 10; ip++) {
-				MessageId from = MessageId.parse("from-0a26000" + ip + "-403899-" + i);
-				MessageId expected = MessageId.parse("from-0a25000" + ip + "-403899-" + i);
+            for (int ip = 0; ip < 10; ip++) {
+                MessageId from = MessageId.parse("from-0a26000" + ip + "-403899-" + i);
+                MessageId expected = MessageId.parse("from-0a25000" + ip + "-403899-" + i);
 
-				MessageId actual = index.find(from);
+                MessageId actual = index.find(from);
 
-				Assert.assertEquals(expected, actual);
-			}
-		}
-	}
+                Assert.assertEquals(expected, actual);
+            }
+        }
+    }
 
 }
